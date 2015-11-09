098fe98cb76f91630e6c3ed8defc83448998488d
RafaelKozar <rafael_kozar@hotmail.com>
2015-11-08 23:13:56 -0200

    camera :)

--- app/src/main/java/camera/plataformapiarobo/camerarobo/MainActivity.java
+++ app/src/main/java/camera/plataformapiarobo/camerarobo/MainActivity.java
@@ -16,21 +16,26 @@ import java.net.URISyntaxException;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import org.xwalk.core.XWalkView;
 
 
 /**
  * Created by Lince on 28/10/2015.
  */
 public class MainActivity extends Activity {
-    private String url = "http://192.168.25.63:3000";
+    //private String url = "http://192.168.25.63:3000";
+    //private String camera = "http://192.168.25.63:3000/camera4";
+    private String camera = "http://104.131.163.197:3000/camera4";
 
+    //192.168.25.63
 
     //0 parar, 1 frente, 2 direita, 3 esquerda, 4 para atraz
-    //private String url = "http://104.131.163.197:3000/";
+    private String url = "http://104.131.163.197:3000/";
     private int comando = 4, antComando = 4;
     private String idRobo;
     private TextView tComando;
     private Button bt;
+    private XWalkView mXWalkView;
 
     private Socket mSocket;
     {
@@ -46,8 +51,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        tComando = (TextView) findViewById(R.id.text_comando);
         String teste = "hahaha";
         mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
         mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
@@ -55,6 +58,9 @@ public class MainActivity extends Activity {
         mSocket.on("comando", onComando);
         mSocket.connect();
         mSocket.emit("enviar", "envioar");
+
+        mXWalkView = (XWalkView) findViewById(R.id.xwalkWebView);
+        mXWalkView.load(camera, null);
 
     }
 
