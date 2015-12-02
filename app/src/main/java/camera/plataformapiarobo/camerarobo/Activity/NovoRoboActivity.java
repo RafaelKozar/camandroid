37261de961208a97186511d0d845d4581c5bbc03
RafaelKozar <rafael_kozar@hotmail.com>
2015-12-02 18:07:35 -0200

    vai

--- app/src/main/java/camera/plataformapiarobo/camerarobo/Activity/NovoRoboActivity.java
+++
@@ -1,9 +0,0 @@
-package camera.plataformapiarobo.camerarobo.Activity;
-
-import android.app.Activity;
-
-/**
- * Created by rafael on 12/11/2015.
- */
-public class NovoRoboActivity extends Activity{
-}
